90a8970239383ed0f9a9f0a9d989e00152ab788b
Komron Khisomov <normok_9595@bk.ru>
2024-08-31 12:08:22 +0500

    natspec

--- src/Call.sol
+++ src/Call.sol
@@ -1,3 +1,4 @@
+// SPDX-Licence-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract MyContract {
@@ -9,12 +10,19 @@ contract MyContract {
 
     }
 
+    // with call method you can send funcds to other contract from this contract if other contract has receive function
     function callReceive() external payable {
         (bool success, ) = otherContract.call{value: msg.value}("");
         require(success, "cant send fund");
 
     }
 
+    /**
+    * also you can send data to other contract if func have calldata argument. 
+    There are two methods of sending data using call:
+    1. call(abi.encodeWithSignature("funcName(dataType)", argument))
+    2. call(abi.encodeWithSelector(ContractName.funcName.selector), argument)) -> you dont need to specify argument type
+     */
     function callSetName(string calldata _name) external {
         (bool success, bytes memory response) = otherContract.call(
             // abi.encodeWithSignature("setName(string), _name);
@@ -31,6 +39,9 @@ contract AnotherContract{
     string public name;
     mapping(address => uint) public balances;
 
+    /**
+    @note: function get data from other contract argument
+     */
     function setName(string calldata _name) external returns(string memory){
         name = _name;
         return name;
